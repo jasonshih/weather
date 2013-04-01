2dca48e924c0ced5fea3ab4822f8f824516c82b4
John Sandall <john.sandall@gmail.om>
2013-04-01 20:32:55 +0100

    Edited procfile to specify production settings; edited settings to adapt to Heroku (some more)

--- weather/settings/prod.py
+++ weather/settings/prod.py
@@ -51,7 +51,7 @@ SERVER_EMAIL = EMAIL_HOST_USER
 ########## DATABASE CONFIGURATION
 # DATABASES = postgresify()
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
